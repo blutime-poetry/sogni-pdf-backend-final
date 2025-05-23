03f215a9c65082e9aad68b621e52584ee7c2971b
blutime <santamaria.umberto@kabelbw.de>
2025-05-24 00:37:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("/", (req, res) => {
   res.send("Server PDF attivo.");
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server avviato sulla porta ${PORT}`);
+  console.log(`Server in ascolto sulla porta ${PORT}`);
 });
